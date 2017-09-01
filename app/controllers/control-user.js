2c21a4a2b8d13142ebc83bbd66c3ce3189e4aa15
daniel-certa-1228 <mfdancerta@gmail.com>
2017-09-01 12:30:16 -0500

    Break for lunch

--- app/controllers/control-user.js
+++ app/controllers/control-user.js
@@ -1,2 +1,23 @@
 "use strict";
 // console.log( "control-user.js" );
+app.controller("userCtrl", function ($scope, $window, userFactory, $location) {
+	console.log( "Yo userCtrl is loaded" );
+
+	$scope.loginGoogle = () => {
+		console.log( "you clicked on google" );
+
+		userFactory.authWithProvider()
+		.then((result) => {
+			let user = result.user.uid;
+			$location.path("/task-list");
+			$scope.apply();
+		}).catch((error) => {
+			console.log( "error with google" );
+			let errorCode = error.code;
+			let errorMessage = error.message;
+			console.log( "error", errorCode, errorMessage );
+		});
+	};
+
+
+});
--- app/factories/factory-user.js
+++ app/factories/factory-user.js
@@ -9,6 +9,20 @@ app.factory("userFactory", function($q, $http){
 
     let currentUser = null;
 
+    const isAuthenticated = function (){
+        console.log("userFactory: isAuthenticated");
+        return new Promise ( (resolve, reject) => {
+            firebase.auth().onAuthStateChanged( (user) => {
+                if (user){
+                    currentUser = user.uid;
+                    console.log("user", user.uid);
+                    resolve(true);
+                }else {
+                    resolve(false);
+                }
+            });
+        });
+    };
 
     const getCurrentUser = function(){
 
@@ -34,8 +48,15 @@ app.factory("userFactory", function($q, $http){
 
     };
 
-    const isAuthenticated = function(){
+    // const isAuthenticated = function(){
 
+    // };
+
+    let provider = new firebase.auth.GoogleAuthProvider();
+
+    let authWithProvider= function(){
+        return firebase.auth().signInWithPopup(provider);
     };
-    return {getCurrentUser, loginGoogle, logIn, logOut, register, isAuthenticated};
+
+    return {getCurrentUser, loginGoogle, logIn, logOut, register, isAuthenticated, authWithProvider};
 });
